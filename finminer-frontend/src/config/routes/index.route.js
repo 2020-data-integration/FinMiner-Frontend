2fc321607c470f230fe14326b53760e180e1a56d
Alexchanchic <1085684645@qq.com>
2020-06-08 17:51:52 +0800

    feat: 完成样式配置

--- finminer-frontend/src/config/routes/index.route.js
+++ finminer-frontend/src/config/routes/index.route.js
@@ -1,10 +1,9 @@
-import { BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Route} from "react-router-dom";
 import React from "react";
-import Homepage from '../../pages/homepage'
-export function  RouteConfig() {
-  return (
-    <BrowserRouter>
-      <Route path='/react' component={Homepage}/>
-    </BrowserRouter>
-  )
-}
+import Homepage from "../../pages/homepage";
+
+export const RouteConfig = () => (
+  <BrowserRouter>
+    <Route path='/react' component={Homepage} />
+  </BrowserRouter>
+);
--- finminer-frontend/src/pages/homepage.js
+++ finminer-frontend/src/pages/homepage.js
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "../logo.svg";
+import '../App.css';
+
 
 class Homepage extends React.Component {
   render() {
